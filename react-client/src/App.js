ab750002482b6256bdc1b5a245a8013710c97690
benedikt-orsolic <benedikt.orsolic@gmail.com>
2024-07-19 21:56:01 +0200

    css updates

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import VideoPlayer from "./components/video-player/VideoPlayer.jsx";
 
 import VideoSelector from "./components/video-selector/VideoSelector.jsx";
-import captionSrc from "./videos/video_1/captions.srt";
-import captionSrcHr from "./videos/video_1/captions_hr.srt";
-import video from "./videos/video_1/clip.mp4";
 
 export default function App() {
   const [currentVideo, setCurrentVideo] = useState(null);
 
   return (
     <div className="App">
-      <VideoSelector setCurrentVideo={setCurrentVideo} />
       {currentVideo != null && (
         <VideoPlayer
           videoSrc={currentVideo.videoSrc}
@@ -24,3 +20,4 @@ export default function App() {
     </div>
   );
 }
+	// <VideoSelector setCurrentVideo={setCurrentVideo} />
